0e096a6c003fa220561f4490d672acb17c77db11
Ramazan Koshekbay <102669812+Ramo999@users.noreply.github.com>
2022-09-16 16:11:44 +0300

    First commit Message

---
+++ src/main/java/com/example/fxlesson2/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.fxlesson2;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!" +
+                "We glad to see you here");
+    }
+}
